e65a2890eefd7d3f86fb5f5acbaaf56d73a8442e
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-06-26 14:45:37 +0100

    WL-798 Copy FCKeditor config to config_rs.js This is the one used when citations is enabled.

--- library/src/webapp/editor/FCKeditor/config_rs.js
+++ library/src/webapp/editor/FCKeditor/config_rs.js
@@ -82,6 +82,7 @@ FCKConfig.Keystrokes = [
 
 
 FCKConfig.ToolbarSets["Default"] = [
+        ['About'],
         ['Source','DocProps','Templates'],
         ['Cut','Copy','Paste','PasteText','PasteWord','SelectAll','RemoveFormat','SpellCheck'],
         ['Undo','Redo','-','Find','Replace'],
@@ -91,10 +92,11 @@ FCKConfig.ToolbarSets["Default"] = [
         ['Link','Unlink','Anchor'],
         ['ResourceSearch','Image','Flash','Table','Rule','Smiley','SpecialChar'],['Style'],
         ['FontFormat','FontName','FontSize'],
-        ['About']
+        ['FitWindow']
 ] ;
 
 FCKConfig.ToolbarSets["Attachments"] = [
+   ['About'],
    ['Source','DocProps','Templates'],
    ['Cut','Copy','Paste','PasteText','PasteWord','SelectAll','RemoveFormat','SpellCheck'],
    ['Undo','Redo','-','Find','Replace'],
@@ -102,22 +104,22 @@ FCKConfig.ToolbarSets["Attachments"] = [
    ['OrderedList','UnorderedList','Outdent','Indent'],
    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','BGColor'],
    ['Link','Unlink','Anchor'],
-   ['ResourceSearch','Image','Flash','Table','Rule','Smiley','SpecialChar'],['Style'],
+   ['ResourceSearch','Image','Flash','Table','Rule','Smiley','SpecialChar'],
    ['FontFormat','FontName','FontSize'],
-   ['About'],
+   ['FitWindow'],
    '/',
    ['SelectAttachmentsCombo','SelectAttachmentsButton']
 ] ;
 
 FCKConfig.ToolbarSets["Basic"] = [
-	['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink','-','About']
+   ['About','-','Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink']
 ] ;
 
 FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','TableCell','Table','Form'] ;
 
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
-FCKConfig.FontNames		= 'Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ;
+FCKConfig.FontNames = 'Trebuchet MS;Arial;Courier New;Times New Roman;serif;sans-serif;cursive;monospace' ; 
 FCKConfig.FontSizes		= '1/xx-small;2/x-small;3/small;4/medium;5/large;6/x-large;7/xx-large' ;
 FCKConfig.FontFormats	= 'p;div;pre;address;h1;h2;h3;h4;h5;h6' ;
 
