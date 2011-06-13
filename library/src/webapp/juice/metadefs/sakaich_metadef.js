18b00f16aab4692b5fca1e5ee445d510e59d8451
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2011-06-13 14:21:31 +0100

    WL-1652 Deal with items that have multiple rft_id values in OpenURLs.

--- library/src/webapp/juice/metadefs/sakaich_metadef.js
+++ library/src/webapp/juice/metadefs/sakaich_metadef.js
@@ -3,17 +3,19 @@ function sakaich_metadef() {
 	var primo_ids = new(Array);
 	var coins = new(Array);
 	$jq('span.Z3988').each(function() {
-		coin = $jq(this).attr('title');
+		var coin = $jq(this).attr('title');
 		coins.push(coin);
-		bib_item = z3988_parse(coin);
+		var bib_item = z3988_parse(coin);
 		isbns.push(unescape(bib_item["rft.isbn"]));
-		bib_id = bib_item["rft_id"];
+		var bid_id = (bib_item["rft_id"] instanceof Array)?bib_item["rft_id"]:[bib_item["rft_id"]];
 		// This assumes that Primo IDs contain a specific string
 		// Not a brilliant way of doing this
-		// We need to ensure one and only one id per item (think about an array of rft_id, filter for primo_library, pick first entry if multiple match)
-		if (bib_id && bib_id.search(/primo_library/) > 0) {
-			primo_ids.push(bib_id);
-		} else {
+		for(var i in bid_id) {
+			if (bid_id[i] && bid_id[i].search(/primo_library/) > 0) {
+				primo_ids.push(bid_id[i]);
+			}
+		}
+		if (primo_ids.length == 0) {
 			// Add an empty ID for those without a Primo ID
 			primo_ids.push("");
 		}
@@ -25,13 +27,21 @@ function sakaich_metadef() {
 }
 
 function z3988_parse(coin) {
-	var openurl_elements = new(Array);
-	var key_value = new(Array);
-	openurl_elements = coin.split('&');
-	var bib_item = new Object;
+	var openurl_elements = coin.split('&');
+	var bib_item = {};
 	for(i=0;i<openurl_elements.length;i++) {
-		key_value = openurl_elements[i].split('=');
-		bib_item[key_value[0]] = key_value[1];
+		var key_value = openurl_elements[i].split('=');
+		var key = key_value[0];
+		var value = key_value[1];
+		if (bib_item[key]) {
+			if (bib_item[key] instanceof Array) {
+				bib_item[key].push(value);
+			} else {
+				bib_item[key] = [bib_item[key], value];
+			}
+		} else {
+			bib_item[key] = value;
+		}
 	}
 	
 	return bib_item;
