334d9ff49e121c1638507f4fec5f5474b41af733
Neelam Bhandari <neelam.bhandari@it.ox.ac.uk>
2016-01-19 14:51:28 +0000

    WL-4209 Added placeholder text to CKEditor.
    Used plugin from
    http://alfonsoml.blogspot.co.uk/2012/04/placeholder-text-in-ckeditor.html
    to add placeholder plugin in the CKEditor.

--- library/src/webapp/editor/ckeditor.launch.js
+++ library/src/webapp/editor/ckeditor.launch.js
@@ -271,8 +271,12 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config, w, h) {
 			 //ckconfig.atd_rpc='//localhost/proxy/spellcheck';
 			 //ckconfig.extraPlugins+="atd-ckeditor,";
 			 //ckconfig.contentsCss = basePath+'/atd-ckeditor/atd.css';
+			 /*added plugin confighelper for placeholder property
+			 more info on http://alfonsoml.blogspot.co.uk/2012/02/configuration-helper-for-ckeditor.html */
 
-			 ckconfig.extraPlugins+="audiorecorder,movieplayer,wordcount,fmath_formula";
+			 ckconfig.extraPlugins+="audiorecorder,movieplayer,wordcount,fmath_formula,confighelper";
+			 //Add default text for placeholder
+			 ckconfig.placeholder = 'We recommend that you base your input on one of the predefined templates.To browse the available templates, you should click on the following icon in the editor\'s toolbar (above):';
 
             // WL-3501 load wl-ck-plugins
             // go through each toolbar...
